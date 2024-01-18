01a9397dded38d77fb6984cbd62bde876bea6bf4
Halex <alessio.dangelo75@gmail.com>
2024-01-18 23:28:39 +0100

    Home added (Still raw)

--- src/main.js
+++ src/main.js
@@ -2,12 +2,34 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/router'
 
-//import { iconsSet as icons } from '@/assets/icons'
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import { faInstagram, faTwitter, faLinkedin, faGithub} from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope} from '@fortawesome/free-solid-svg-icons';
+import { } from '@fortawesome/free-regular-svg-icons';
+
+library.add(faInstagram, faTwitter, faLinkedin, faGithub, faEnvelope); // Aggiunta icone
+
+
+//Particles.vue3 -https://github.com/tsparticles/tsparticles?tab=readme-ov-file#tsparticles---typescript-particles
+import Particles from "@tsparticles/vue3";
+import { loadFull } from "tsparticles";
+//import { loadSlim } from "@tsparticles/slim";
 
 // styles
 import "@/styles/style.scss";
 
 const app = createApp(App)
 app.use(router)
-
+app.use(Particles, {
+    init: async engine => {
+      await loadFull(engine); // you can load the full tsParticles library from "tsparticles" if you need it
+      //await loadSlim(engine); // or you can load the slim version from "@tsparticles/slim" if don't need Shapes or Animations
+    },
+  })
+app.component('font-awesome-icon', FontAwesomeIcon) //Icone
 app.mount('#app')
--- src/views/myHome.vue
+++ src/views/myHome.vue
@@ -1,21 +1,40 @@
 <template>
-    <div class="my-page">
-    <div class="center-content">
-      <h1>Alessio Vito D'angelo</h1>
-      <div class="social-icons">
-        <div class="social-icon">
-          <i class="fab fa-facebook"></i>
+    <div>
+        <div class="my-page min-h-screen flex items-center justify-center bg-gray-100">
+            <ParticleBackground />
+            <div class="text-center">
+                <h1 class="text-4xl font-bold mb-4 text-white">Alessio Vito D'angelo</h1>
+                <div class="flex space-x-4">
+                <div class="social-icon">
+                    <font-awesome-icon :icon="['fab', 'instagram']" class="text-4xl text-white" />
+                </div>
+                <div class="social-icon">
+                    <font-awesome-icon :icon="['fab', 'twitter']" class="text-4xl text-white" />
+                </div>
+                <div class="social-icon">
+                    <font-awesome-icon :icon="['fab', 'linkedin']" class="text-4xl text-white" />
+                </div>
+                <div class="social-icon">
+                    <font-awesome-icon :icon="['fab', 'github']" class="text-4xl text-white" />
+                </div>
+                <div class="social-icon">
+                    <font-awesome-icon :icon="['fas', 'envelope']" class="text-4xl text-white" />
+                </div>
+                </div>
+            </div>
         </div>
-        <div class="social-icon">
-          <i class="fab fa-twitter"></i>
-        </div>
-        <div class="social-icon">
-          <i class="fab fa-linkedin"></i>
-        </div>
-        <div class="social-icon">
-          <i class="fab fa-github"></i>
-        </div>
-      </div>
+        
     </div>
-  </div>
 </template>
+
+<script>
+//import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import ParticleBackground from '@/components/ParticleBackground.vue';
+
+export default {
+  components: {
+    //FontAwesomeIcon,
+    ParticleBackground,
+  },
+};
+</script>
